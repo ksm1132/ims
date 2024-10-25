86fd4cb58526e81740bf7cf8968e2c4d91b41376
mkbot <mkasamatsu@ashisuto.co.jp>
2024-10-25 14:45:47 +0900

    application-staging.properties修正 CustomHeadInterceptor新規 SecurityConfig修正 WebConfig新規

--- src/main/java/com/ims/inventorymgmtsys/config/SecurityConfig.java
+++ src/main/java/com/ims/inventorymgmtsys/config/SecurityConfig.java
@@ -49,6 +49,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .requiresChannel()
+                    .anyRequest()
+                    .requiresSecure()
+                .and()
                 .authorizeHttpRequests()
                     .requestMatchers("/fragments/**","/js/**","/css/**","/images/**","/favicon.ico").permitAll()
                     .requestMatchers("/login", "/register","/forget","/password","/changePasswordNoLogin").permitAll()
--- src/main/java/com/ims/inventorymgmtsys/config/WebConfig.java
+++ src/main/java/com/ims/inventorymgmtsys/config/WebConfig.java
@@ -1,8 +1,16 @@
 package com.ims.inventorymgmtsys.config;
 
+import jakarta.servlet.DispatcherType;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,5 +20,20 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("classpath:/static/images/")
                 .setCachePeriod(0);  // キャッシュを無効化
+    }
+
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+        FilterRegistrationBean<ForwardedHeaderFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE); // 高優先度で実行
+        registration.setUrlPatterns(List.of("/*")); // アプリケーションの全てのリクエストに適用
+        return registration;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CustomHeaderInterceptor()); // 先ほどのカスタムヘッダーインターセプター
     }
 }
